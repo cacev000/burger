8900e2ac81e571c31500be78fc7a763054ef4a16
carlos <cacev000@gmail.com>
2019-03-20 18:00:15 -0400

    handlebars homework

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double cheese burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Bacon cheese burger', true);
+
+INSERT INTO  burgers (burger_name, devoured)
+VALUES ('Vegetarian burger', false);
